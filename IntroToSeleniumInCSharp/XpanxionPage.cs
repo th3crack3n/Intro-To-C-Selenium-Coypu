a30c12bc3f2abce8d4520dd1088c6979c04d58a6
Sean McCracken <smccracken@xpanxion.com>
2016-08-01 16:05:54 -0500

    pulled xpanxion into object model

---
+++ IntroToSeleniumInCSharp/XpanxionPage.cs
@@ -0,0 +1,77 @@
+﻿using Coypu;
+using Coypu.NUnit.Matchers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace IntroToSeleniumInCSharp
+{
+
+    class XpanxionPage
+    {
+        private BrowserSession browser;
+
+        private string
+            home = "http://www.xpanxion.com",
+            post = "/contact.html",
+            name_field = "widgetu1680_input",
+            email_field = "widgetu1684_input";
+
+        public XpanxionPage(BrowserSession browser)
+        {
+            this.browser = browser;
+            browser.MaximiseWindow();
+        }
+
+        public void load()
+        {
+            browser.Visit(home + post);
+        }
+
+        public void fillName(string name = "")
+        {
+            browser.FillIn(name_field).With(name);
+        }
+
+        public void fillEmail(string email = "")
+        {
+            browser.FillIn(email_field).With(email);
+        }
+
+        public void emptyFields()
+        {
+            fillName();
+            fillEmail();
+        }
+        
+        private string getNameValue()
+        {
+            return browser.FindField(name_field).Value;
+        }
+
+        private string getEmailValue()
+        {
+            return browser.FindField(email_field).Value;
+        }
+
+        public void verifyDataPresent(bool dataPresent)
+        {
+            // true if desiring data, false otherwise
+            if (dataPresent == true)
+            {
+                Assert.IsNotEmpty(getNameValue());
+                Assert.IsNotEmpty(getEmailValue());
+            }
+            else
+            {
+                Assert.IsEmpty(getNameValue());
+                Assert.IsEmpty(getEmailValue());
+            }
+        }
+
+
+    }
+}
